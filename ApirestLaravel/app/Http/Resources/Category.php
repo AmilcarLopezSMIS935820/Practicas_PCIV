fbb13c34f2b6709b0a0fb08b815c7d4a1b4fed2a
AmilcarLopezSMIS935820 <josueamilcar1013@gmail.com>
2022-04-10 18:02:25 -0600

    Practica semana 12

---
+++ ApirestLaravel/app/Http/Resources/Category.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class Category extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return parent::toArray($request);
+        //En la grabación no se ve que se edito aca, asi que no me funciona el filtrado :c
+    }
+}
